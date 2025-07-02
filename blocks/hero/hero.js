0528063ae7a59a2c84e001bf8850fdedd8505dd2
Chris Scholte <chris.scholte@digitalum.eu>
2025-07-02 10:54:12 +0200

    Remove async from javascript function.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,7 +2,7 @@
  * loads and decorates the hero
  * @param {Element} block The hero block element
  */
-export default async function decorate(block) {
+export default function decorate(block) {
   block.querySelector('> div:nth-child(1)').classList.add('hero__image');
   block.querySelector('> div:nth-child(2)').classList.add('hero__text');
   block.querySelector('> div:nth-child(3)').classList.add('hero__buttons');
