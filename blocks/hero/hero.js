768e11df9d0a22b9ad03169271bc5f3c6ba4de76
Chris Scholte <chris.scholte@digitalum.eu>
2025-07-02 10:49:33 +0200

    Add javascript decoration to hero.

---
+++ blocks/hero/hero.js
@@ -0,0 +1,9 @@
+/**
+ * loads and decorates the hero
+ * @param {Element} block The hero block element
+ */
+export default async function decorate(block) {
+  block.querySelector('div:nth-child(1)').classList.add('hero__image');
+  block.querySelector('div:nth-child(2)').classList.add('hero__text');
+  block.querySelector('div:nth-child(3)').classList.add('hero__buttons');
+}
