e5a5f6170e77e087ada10896016f7db45dd1d065
Chris Scholte <chris.scholte@digitalum.eu>
2025-07-02 12:05:56 +0200

    Add hero styling for image.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -6,4 +6,5 @@ export default function decorate(block) {
   block.querySelector(':scope > div:nth-child(1)').classList.add('hero__image');
   block.querySelector(':scope > div:nth-child(2)').classList.add('hero__text');
   block.querySelector(':scope > div:nth-child(3)').classList.add('hero__buttons');
+  block.querySelector('.hero__buttons .button-container:nth-child(2) a').classList.add('secondary');
 }
