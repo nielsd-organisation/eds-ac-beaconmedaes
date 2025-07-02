3c7c4f1affa57c023c8509e1e580821198f47119
Chris Scholte <chris.scholte@digitalum.eu>
2025-07-02 10:52:55 +0200

    Add test class to hero for debugging.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -6,4 +6,5 @@ export default async function decorate(block) {
   block.querySelector('> div:nth-child(1)').classList.add('hero__image');
   block.querySelector('> div:nth-child(2)').classList.add('hero__text');
   block.querySelector('> div:nth-child(3)').classList.add('hero__buttons');
+  block.querySelector('div').classList.add('test-class');
 }
