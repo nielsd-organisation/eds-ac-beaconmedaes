a72eed42fad5e0d4c9e3e08d18d7773450ca14ff
Chris Scholte <chris.scholte@digitalum.eu>
2025-07-02 10:50:25 +0200

    Fix div selectors for hero javascript.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,7 +3,7 @@
  * @param {Element} block The hero block element
  */
 export default async function decorate(block) {
-  block.querySelector('div:nth-child(1)').classList.add('hero__image');
-  block.querySelector('div:nth-child(2)').classList.add('hero__text');
-  block.querySelector('div:nth-child(3)').classList.add('hero__buttons');
+  block.querySelector('> div:nth-child(1)').classList.add('hero__image');
+  block.querySelector('> div:nth-child(2)').classList.add('hero__text');
+  block.querySelector('> div:nth-child(3)').classList.add('hero__buttons');
 }
