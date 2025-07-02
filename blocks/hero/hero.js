d8e04a57da56624e890644615343210bd44206b2
Chris Scholte <chris.scholte@digitalum.eu>
2025-07-02 10:59:06 +0200

    Fix faulty selectors in hero javascript.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,8 +3,7 @@
  * @param {Element} block The hero block element
  */
 export default function decorate(block) {
-  block.querySelector('> div:nth-child(1)').classList.add('hero__image');
-  block.querySelector('> div:nth-child(2)').classList.add('hero__text');
-  block.querySelector('> div:nth-child(3)').classList.add('hero__buttons');
-  block.querySelector('div').classList.add('test-class');
+  block.querySelector(':scope > div:nth-child(1)').classList.add('hero__image');
+  block.querySelector(':scope > div:nth-child(2)').classList.add('hero__text');
+  block.querySelector(':scope > div:nth-child(3)').classList.add('hero__buttons');
 }
