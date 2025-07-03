d3534831f13ebaa1a0f45f345c9db9914d7726b3
Chris Scholte <chris.scholte@digitalum.eu>
2025-07-03 15:19:32 +0200

    Add contact card styling.

---
+++ blocks/contact-card/contact-card.js
@@ -0,0 +1,7 @@
+/**
+ * loads and decorates the contact card
+ * @param {Element} block The contact card block element
+ */
+export default function decorate(block) {
+  block.querySelector('a').classList.add('secondary');
+}
